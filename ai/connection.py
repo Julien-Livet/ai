4bd84fbb5ccb4a42b51020a70085d60103d69c73
Julien LIVET <julien.livet@free.fr>
2025-09-24 18:54:54 +0200

    Add input values function for connection

--- ai/connection.py
+++ ai/connection.py
@@ -40,6 +40,19 @@ class Connection:
 
         return types
 
+    def origin_input_values(self):
+        values = []
+
+        for input in self.inputs:
+            if (isinstance(input, Neuron) and len(input.inputTypes) == 0):
+                values.append(input.output())
+            elif (isinstance(input, Connection)):
+                values.extend(input.origin_input_values())
+            else:
+                values.append(input)
+
+        return values
+
     def is_active(self):
         if (self.activationDuration == math.inf):
             return True
